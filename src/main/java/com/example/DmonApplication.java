cc598a93e13d585985f7408e40cc2b38bd840f09
Deya Motawie <deya.motawie@santos.com>
2016-08-19 12:01:51 +0930

    Initial Version

---
+++ src/main/java/com/example/DmonApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmonApplication.class, args);
+	}
+}
